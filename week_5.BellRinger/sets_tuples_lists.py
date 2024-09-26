dd5955cc92bb3608d492095b078749f581da0991
Gutierrez <gygutierrez1@cps.k12.il.us>
2024-09-26 08:31:51 -0500

    first commit

---
+++ week_5.BellRinger/sets_tuples_lists.py
@@ -0,0 +1,23 @@
+#collection = single "variable" used to store multiple values
+# list = [] ordered and changeable duplicates OK
+# set = [] unordered and immutable, but ADD/REMOVE OK, NO duplicates
+# Tuple = () ordered ab=nd unchangeable , Duplicates OK 
+
+fruits = ["apple", "orange", "banana" ,"coconut"]
+# print(fruits{0:3})
+for fruit in fruits:
+    print(fruit)
+
+#help(fruits)
+#print(help(fruits))
+print(len(fruits))
+
+
+# you can reassign value using this:
+fruit (0)= "pineapple"
+print (fruit)
+
+fruits.remove("apple")
+fruits. reverse()
+print (fruits)
+
