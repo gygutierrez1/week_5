e79bba80a5700e04be7a0ced8fd6efa7cbafa49b
Gutierrez <gygutierrez1@cps.k12.il.us>
2024-09-26 09:23:35 -0500

    whatever changes you worked on

--- week_5.BellRinger/sets_tuples_lists.py
+++ week_5.BellRinger/sets_tuples_lists.py
@@ -3,21 +3,64 @@
 # set = [] unordered and immutable, but ADD/REMOVE OK, NO duplicates
 # Tuple = () ordered ab=nd unchangeable , Duplicates OK 
 
-fruits = ["apple", "orange", "banana" ,"coconut"]
+#fruits = ["apple", "orange", "banana" ,"coconut"]
 # print(fruits{0:3})
-for fruit in fruits:
-    print(fruit)
+#for fruit in fruits:
+ #   print(fruit)
 
 #help(fruits)
 #print(help(fruits))
-print(len(fruits))
+#print(len(fruits))
 
 
 # you can reassign value using this:
-fruit (0)= "pineapple"
-print (fruit)
+#print (fruit)
 
-fruits.remove("apple")
-fruits. reverse()
-print (fruits)
+#fruits.remove("apple")
+#fruits. reverse()
+#print (fruits)
 
+
+
+
+#cars = ("bmw", "maserati", "audi", "mercedes", "ferrari")
+#print (f"these are list of cars:",(cars))
+#cars.append("buggatti")
+#print(cars)
+#cars.remove ("maserati")
+#print(cars)
+
+#for car in cars:
+   # print(car)
+    #print(len(car))
+    #carRequest = input = "add a ne w car please"
+    #cars.append(carRequest)
+
+
+
+
+
+# challenge 
+    #create a list of friends 
+    # make sure the ini8tial list is none
+
+friends = []
+friends.append("lily")
+friends.append("beth")
+friends.append("jennifer")
+friends.append("julie")
+friends.append("abby")
+print(friends)
+# add a new friend to the ,list  add at least 5 friends
+#remove a a friend
+friends.remove("jennifer")
+# insert a friend at specific index maybe 2 
+friends.insert(2, 'ashley')
+print(friends)
+#print the list of friends
+# loop through the whole
+for friend in friends:
+    print(len(friends))
+    print(friend)
+
+    
